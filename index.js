3037ad2aae0d1a6068c1f38bec4ce8b9020e1ccc
shukri1236 <shukri1236>
2024-07-04 22:01:18 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+
+}
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+
+}
+mondayWork();
+
+let wrapAdjective = function(flair = "*") {
+    return function(adjective){
+            return `You are ${flair}${adjective}${flair}!`;
+    }
+}
